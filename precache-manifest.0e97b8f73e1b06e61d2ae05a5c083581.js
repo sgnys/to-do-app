334ed9450fb2ccd8431eadee89fb66495e49365a
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2020-05-14 12:55:55 +0200

    Updates

---
+++ precache-manifest.0e97b8f73e1b06e61d2ae05a5c083581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4234597dae6fc01448653c87ece4f77",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "a5805b3cce916c5dc6c8",
+    "url": "/to-do-app/static/css/main.92b9e9d6.chunk.css"
+  },
+  {
+    "revision": "d15ca8c23ca4a2f52519",
+    "url": "/to-do-app/static/js/2.a6e8bd56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.a6e8bd56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5805b3cce916c5dc6c8",
+    "url": "/to-do-app/static/js/main.c35aa2cd.chunk.js"
+  },
+  {
+    "revision": "eb316f28f1e510376002",
+    "url": "/to-do-app/static/js/runtime-main.23737453.js"
+  }
+]);
